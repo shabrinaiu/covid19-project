f3f5800d56a1ac2076409eea4adebd67f2e870c0
Ulima Inas Shabrina <nanaswarnakuning@gmail.com>
2020-07-06 22:37:46 +0700

    Landing Page Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600,700,900" rel="stylesheet">
 
         <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
         <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
@@ -15,81 +15,73 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-image: url(/theme/img/virusvirus.svg);
+                background-attachment: fixed;
+                background-position: center center;
+                background-size: cover; 
+                color: #e7ecf7;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
+                background-size: 100%;
                 margin: 0;
+                vertical-align: middle;
+                overflow: hidden;
             }
-
-            .full-height {
+            .centered {
                 height: 100vh;
+                vertical-align : middle;
+                position:absolute;
+                top:10%;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
+                font-size: 2em;
+                letter-spacing: 2px;
                 font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+                vertical-align: middle;
+                max-width: 65em;
+                width: calc(100% - 6em);
+                margin: 0 auto;
+                position: relative;
+                z-index: 10000;
+                line-height: 1;
             }
+            h4{
+                left: 5%;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 54px;
+                line-height: 66px;
 
-            .m-b-md {
-                margin-bottom: 30px;
+                /* identical to box height */
+                letter-spacing: 0.02em;
+                text-transform: uppercase;
+
+                color: #E8EFF9;
+            }
+            h3{           
+                left: 5%;
+                font-style: normal;
+                font-weight: bold;
+                font-size: 54px;
+                line-height: 66px;
+                letter-spacing: 0.02em;
+                text-transform: uppercase;
+
+                color: #FFFFFF;
+            }
+            .content > a {
+                color : #fff;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+        <div class="centered">
             <div class="content">
-                <a href="{{ route('reports.index') }}"><h4>click here to redirect to Reporting covid-19 data</h4></a>
-                
+                <h4>WELCOME TO</h4>
+                <h3>COVID-19<br>HEALTH INFORMATION</h3>
+                <a href="{{ route('reports.index') }}"><h6>click here to redirect to Reporting covid-19 data</h6></a>
             </div>
         </div>
-
         <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-
     </body>
 </html>
