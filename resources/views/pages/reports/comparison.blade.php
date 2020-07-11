f9ba6a314ab5e92a6baa882d08d0e0009400ba41
satriyoaji <satriyoaji27@gmail.com>
2020-07-12 02:36:13 +0700

    modif JS

--- resources/views/pages/reports/comparison.blade.php
+++ resources/views/pages/reports/comparison.blade.php
@@ -12,7 +12,7 @@
             <h3>Covid-19 Statistics data of {{(isset($currentData['Country_Region']) ? $currentData['Country_Region'] : 'a country')}}</h3>
         </div>
         <div class="col-md-3">
-            <select class="select2_demo_3 form-control country">
+            <select class="select2_country form-control country">
                 @isset($data)
                     @foreach ($data as $row)
                         <option value="" disabled selected></option>
@@ -45,11 +45,11 @@
                     </div>
                 </div>
                 <div class="ibox-footer">
-                    <div class="row wrapper">
+                    <div class="row wrapper justify-content-around align-items-start">
                         <div class="col-md-6">
                             <div class="form-group row">
-                                <div class="col-md-5">
-                                    <select class="select2_demo_2 form-control countries" multiple="multiple">
+                                <div class="col-sm-8">
+                                    <select class="select2_countries form-control countries" multiple="multiple">
                                         @isset($data)
                                             @foreach ($data as $row)
                                                 <option value="" disabled></option>
@@ -58,23 +58,16 @@
                                         @endisset
                                     </select>
                                 </div>
-                                <div class="col-md-1">
-                                    <button type="button" class="btn btn-primary" id="submitButton">Submit</button>
-                                </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 justify-content-end align-items-end">
                             <div class="form-group row">
-                                <div class="col-md-5">
+                                <div class="col-sm-8">
                                     <div class="input-daterange input-group" id="datepicker">
                                         <input type="text" class="form-control-sm form-control" id="start-date" name="start" value="05/14/2014"/>
                                         <span class="input-group-addon">to</span>
                                         <input type="text" class="form-control-sm form-control" id="end-date" name="end" value="05/22/2014" />
-                                        
                                     </div>
-                                </div>
-                                <div class="col-md-1">
-                                    <button type="button" class="btn btn-primary" id="submitButton">Submit</button>
                                 </div>
                             </div>
                         </div>
@@ -84,110 +77,112 @@
         </div>
     </div>
 
-    <div class="row" id="table-stats">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h4 class="text-center" id="TableTitle">Confirmed Comparison</h4>
+    <div id="contentWrapper">
+        <div class="row" id="table-stats">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <h4 class="text-center" id="TableTitle">Confirmed Comparison</h4>
 
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
+                        <div class="ibox-tools">
+                            <a class="collapse-link">
+                                <i class="fa fa-chevron-up"></i>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="ibox-footer">
-                    <div class="table-responsive">
-                        <table id="" class="table table-hover text-center dataTables-example">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Date</th>
-                                    <th>Country A</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>#</td>
-                                    <td>Date</td>
-                                    <th>Country A</th>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    
-    <div class="row" id="table-stats">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h4 class="text-center" id="TableTitle">Recovered Comparison</h4>
-
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="ibox-footer">
-                    <div class="table-responsive">
-                        <table id="" class="table table-hover text-center dataTables-example">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Date</th>
-                                    <th>Country A</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>#</td>
-                                    <td>Date</td>
-                                    <th>Country A</th>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="ibox-footer">
+                        <div class="table-responsive">
+                            <table id="" class="table table-hover text-center dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Date</th>
+                                        <th>Country A</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>Date</td>
+                                        <th>Country A</th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    
-    <div class="row" id="table-stats">
-        <div class="col-lg-12">
-            <div class="ibox ">
-                <div class="ibox-title">
-                    <h4 class="text-center" id="TableTitle">Deaths Comparison</h4>
+        
+        <div class="row" id="table-stats">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <h4 class="text-center" id="TableTitle">Recovered Comparison</h4>
 
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
+                        <div class="ibox-tools">
+                            <a class="collapse-link">
+                                <i class="fa fa-chevron-up"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="ibox-footer">
+                        <div class="table-responsive">
+                            <table id="" class="table table-hover text-center dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Date</th>
+                                        <th>Country A</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>Date</td>
+                                        <th>Country A</th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <div class="ibox-footer">
-                    <div class="table-responsive">
-                        <table id="" class="table table-hover text-center dataTables-example">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Date</th>
-                                    <th>Country A</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>#</td>
-                                    <td>Date</td>
-                                    <th>Country A</th>
-                                </tr>
-                            </tbody>
-                        </table>
+            </div>
+        </div>
+
+        
+        <div class="row" id="table-stats">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <h4 class="text-center" id="TableTitle">Deaths Comparison</h4>
+
+                        <div class="ibox-tools">
+                            <a class="collapse-link">
+                                <i class="fa fa-chevron-up"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="ibox-footer">
+                        <div class="table-responsive">
+                            <table id="" class="table table-hover text-center dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Date</th>
+                                        <th>Country A</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>Date</td>
+                                        <th>Country A</th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -202,14 +197,22 @@
     <script>
         
         $(document).ready(function () {
-            $('#data_5 .input-daterange').datepicker({
+            $('#contentWrapper').hide();
+
+            $(".select2_country").select2({
+                theme: 'bootstrap4',
+                placeholder: "Select a target country",
+                // allowClear: true
+            });
+
+            $('.input-daterange').datepicker({
                 keyboardNavigation: false,
                 forceParse: false,
                 autoclose: true
             });
 
-            $(".select2_demo_2").select2({
-                placeholder: "Select a country",
+            $(".select2_countries").select2({
+                placeholder: "Select countries to compare",
             });
 
             $("#submitButton").click(function(){
