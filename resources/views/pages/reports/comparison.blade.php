3b0e82d2399bbae7646daeb64aa40cf3feb6e822
Ulima Inas Shabrina <nanaswarnakuning@gmail.com>
2020-07-11 22:40:35 +0700

    Page Comparison

--- resources/views/pages/reports/comparison.blade.php
+++ resources/views/pages/reports/comparison.blade.php
@@ -7,17 +7,17 @@
 @endpush
 
 @section('page-heading')
-    <div class="row wrapper border-bottom white-bg page-heading">
+    <div class="row wrapper page-heading">
         <div class="col-md-8 vertical-align-middle">
             <h3>Covid-19 Statistics data of {{(isset($currentData['Country_Region']) ? $currentData['Country_Region'] : 'a country')}}</h3>
         </div>
         <div class="col-md-3">
-            <select class="select2_demo_2 form-control countries" multiple="multiple">
+            <select class="select2_demo_3 form-control country">
                 @isset($data)
                     @foreach ($data as $row)
-                        <option value="" disabled></option>
+                        <option value="" disabled selected></option>
                         <option value="{{$row['Slug']}}">{{$row['Country']}}</option>
-                    @endforeach
+                    @endforeach                    
                 @endisset
             </select>
         </div>
@@ -30,10 +30,10 @@
 @section('content')
 
     <div class="row" id="chart1">
-        <div class="col-lg-12">
+        <div class="col-md-12">
             <div class="ibox ">
                 <div class="ibox-title">
-                    <h5>Confirmed data history</h5>
+                    <h5>Choose Input</h5>
                     <div class="ibox-tools">
                         <a class="collapse-link">
                             <i class="fa fa-chevron-up"></i>
@@ -45,12 +45,38 @@
                     </div>
                 </div>
                 <div class="ibox-footer">
-                    <div class="form-group" id="data_5">
-                        <label class="font-normal">Range select</label>
-                        <div class="input-daterange input-group" id="datepicker">
-                            <input type="text" class="form-control-sm form-control" id="start-date" name="start" value="05/14/2014"/>
-                            <span class="input-group-addon">to</span>
-                            <input type="text" class="form-control-sm form-control" id="end-date" name="end" value="05/22/2014" />
+                    <div class="row wrapper">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <div class="col-md-5">
+                                    <select class="select2_demo_2 form-control countries" multiple="multiple">
+                                        @isset($data)
+                                            @foreach ($data as $row)
+                                                <option value="" disabled></option>
+                                                <option value="{{$row['Slug']}}">{{$row['Country']}}</option>
+                                            @endforeach
+                                        @endisset
+                                    </select>
+                                </div>
+                                <div class="col-md-1">
+                                    <button type="button" class="btn btn-primary" id="submitButton">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <div class="col-md-5">
+                                    <div class="input-daterange input-group" id="datepicker">
+                                        <input type="text" class="form-control-sm form-control" id="start-date" name="start" value="05/14/2014"/>
+                                        <span class="input-group-addon">to</span>
+                                        <input type="text" class="form-control-sm form-control" id="end-date" name="end" value="05/22/2014" />
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-md-1">
+                                    <button type="button" class="btn btn-primary" id="submitButton">Submit</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -58,20 +84,115 @@
         </div>
     </div>
 
-    @isset($historyData)
-        @component('components.confirmedChart', ['historyData' => $historyData, 'data' => $data])
-        @endcomponent
+    <div class="row" id="table-stats">
+        <div class="col-lg-12">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h4 class="text-center" id="TableTitle">Confirmed Comparison</h4>
 
-        @component('components.recoveredChart', ['historyData' => $historyData, 'data' => $data])
-        @endcomponent
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="ibox-footer">
+                    <div class="table-responsive">
+                        <table id="" class="table table-hover text-center dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Country A</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Date</td>
+                                    <th>Country A</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        @component('components.deathChart', ['historyData' => $historyData, 'data' => $data])
-        @endcomponent
+    
+    <div class="row" id="table-stats">
+        <div class="col-lg-12">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h4 class="text-center" id="TableTitle">Recovered Comparison</h4>
 
-        @component('components.tableStats', ['historyData' => $historyData])
-        @endcomponent
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="ibox-footer">
+                    <div class="table-responsive">
+                        <table id="" class="table table-hover text-center dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Country A</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Date</td>
+                                    <th>Country A</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    @endisset
+    
+    <div class="row" id="table-stats">
+        <div class="col-lg-12">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h4 class="text-center" id="TableTitle">Deaths Comparison</h4>
+
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="ibox-footer">
+                    <div class="table-responsive">
+                        <table id="" class="table table-hover text-center dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Country A</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Date</td>
+                                    <th>Country A</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     @push('footer-scripts')
     <!-- ChartJS -->
