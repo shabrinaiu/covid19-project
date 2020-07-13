35b153959e928bf7ae9575899af6d840ae1324d4
satriyoaji <satriyoaji27@gmail.com>
2020-07-13 16:40:51 +0700

    modify judul

--- resources/views/pages/reports/comparison.blade.php
+++ resources/views/pages/reports/comparison.blade.php
@@ -11,7 +11,7 @@
 @section('page-heading')
     <div class="row wrapper page-heading">
         <div class="col-md-12 vertical-align-middle">
-            <h2>Perbandingan Data Banyak Negara</h2>
+            <h2>Correlation Data of Countries</h2>
         </div>
     </div>
     <div class="row wrapper page-heading">
