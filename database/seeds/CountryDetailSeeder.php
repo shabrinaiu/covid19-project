a1d403b9147a1c35dec36a9645b725202b85bd43
satriyoaji <satriyoaji27@gmail.com>
2020-11-30 13:28:32 +0700

    modify way to seed CountryDetail info

--- database/seeds/CountryDetailSeeder.php
+++ database/seeds/CountryDetailSeeder.php
@@ -14,6 +14,49 @@ class CountryDetailSeeder extends Seeder
      */
     public function run()
     {
+        $this->getAll();
+    }
+
+    private function getAll()
+    {
+        $queryCountries = Country::all();
+
+        foreach ($queryCountries as $i => $country) {
+            //fetching All data in the contry
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.covid19api.com/country/" . $country["slug"],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_CUSTOMREQUEST => "GET",
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            $fetchedArr = json_decode($response, TRUE);
+
+            if (isset($fetchedArr)) {
+                foreach ($fetchedArr as $i => $fetched) {
+                    if ($fetched['Confirmed'] > 0) {
+                        DB::table('country_details')->insert([
+                            'country_slug' => $country["slug"],
+                            'province' => (isset($fetched['Province']) ? $fetched['Province'] : null),
+                            'date' => $fetched['Date'],
+                            'confirmed' => $fetched['Confirmed'],
+                            'recovered' => $fetched['Recovered'],
+                            'deaths' => $fetched['Deaths'],
+                        ]);
+                    }
+                }
+            }
+        }
+    }
+
+    private function getRemains()
+    {
         $queryCountries = Country::all();
         $countryDetails = CountryDetail::select('country_slug')->distinct('country_slug')->get();
 
